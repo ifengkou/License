0209bf3d44123063915dca86f1b10b3f1ed0f808
sloong <ifengkou@hotmail.com>
2019-02-14 11:23:14 +0800

    remove 无效引用

--- Licence.Master/License.Test/Program.cs
+++ Licence.Master/License.Test/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ZLERP.Licensing.Client;
 using License.Base;
 namespace License.Test
 {
